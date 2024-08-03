eb816682c831223fde8dcb1b20ac95c7eaadc9cd
imantoafiif <imantoafiif@gmail.com>
2024-08-03 14:07:24 +0700

    bye bye bye

--- components/Sections/List2/PhoneContent/index.js
+++ components/Sections/List2/PhoneContent/index.js
@@ -1,88 +1,37 @@
 "use client"
 
-import useEmblaCarousel from 'embla-carousel-react';
-import Autoplay from 'embla-carousel-autoplay';
 import classes from './PhoneContent.module.css';
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useCarouselControl } from '@/hooks/useCarouselControl';
+
+import EWM from '../../../../public/images/list/EWM.svg';
+import WM from '../../../../public/images/list/WM.svg';
+import MM from '../../../../public/images/list/MM.svg';
 
 const PhoneContent = () => {
-    const [autoplayTimeout, setAutoplayTimeout] = useState(null);
-    const [currentScreen, setCurrentScreen] = useState(0);
-    
-    const [emblaRef, emblaApi] = useEmblaCarousel({
-        loop: true,
-        duration: 20,
-        watchDrag: true,
-        breakpoints: {
-            "(min-width: 128px)": {
-                watchDrag: true,
-            },
-        },
-    }, [Autoplay({
-        delay: 5000,
-        stopOnInteraction: false, 
-        stopOnMouseEnter: false, 
-        rootNode: (emblaRoot) => emblaRoot.parentElement,
-    })]);
-
-    const onButtonClick = (emblaApi, page) => {
-        if(!emblaApi) return
-        const autoPlay = emblaApi?.plugins()?.autoplay;
-
-        if(!autoPlay) return
-
-        autoPlay.stop();
-        emblaApi.scrollTo(page);
-        setCurrentScreen(page);
-        
-        if(autoplayTimeout) {
-            clearTimeout(autoplayTimeout);
-        }
-
-        const autoplayResumeTimeout = setTimeout(() => {
-            autoPlay.play();
-            emblaApi.scrollNext();
-        }, 5000);
-        setAutoplayTimeout(autoplayResumeTimeout);
-    }
-
-    useEffect(() => {
-        if(!emblaApi) return
-        emblaApi.on('select', () => setCurrentScreen(emblaApi.selectedScrollSnap()));
-    }, [emblaApi])
+    const screens = [EWM, WM, MM];
+    const interval = 1000;
+    const { onButtonClick, currentScreen, emblaRef } = useCarouselControl(interval);
 
     return (
-        <div className={classes.daktau}>
+        <div className={classes['content-wrapper']}>
             <div ref={emblaRef} className={classes['content-viewport']}>
                 <div className={classes['content-container']}>
-                    <div className={classes['content-slide']}>
-                        <Image
-                            src={`/images/list/EWM.svg`}
-                            width={264}
-                            height={468}
-                            alt="German Team"
-                            unoptimized
-                        />
-                    </div>
-                    <div className={classes['content-slide']}>
-                        <Image
-                            src={`/images/list/WM.svg`}
-                            width={264}
-                            height={468}
-                            alt="German Team"
-                            unoptimized
-                        />
-                    </div>
-                    <div className={classes['content-slide']}>
-                        <Image
-                            src={`/images/list/MM.svg`}
-                            width={264}
-                            height={468}
-                            alt="German Team"
-                            unoptimized
-                        />
-                    </div>
+                    {
+                        screens.map((item, key) => (
+                            <div
+                                key={key} 
+                                className={classes['content-slide']}>
+                                <Image
+                                    src={item}
+                                    width={264}
+                                    height={468}
+                                    alt={`screen-${key}`}
+                                    unoptimized
+                                />
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
             <div className={classes['control-wrapper']}>
@@ -90,7 +39,7 @@ const PhoneContent = () => {
                     [0, 1, 2].map((_, index) => (
                         <div 
                             key={index}
-                            onClick={() => onButtonClick(emblaApi, index)} 
+                            onClick={() => onButtonClick(index)} 
                             className={`${classes['control-item']} ${currentScreen == index ? classes['active'] : null}`}>
                         </div>
                     ))
--- hooks/useCarouselControl.js
+++ hooks/useCarouselControl.js
@@ -1,3 +1,56 @@
-export const useCarouselControl = () => {
-    
+import useEmblaCarousel from 'embla-carousel-react';
+import Autoplay from 'embla-carousel-autoplay';
+import { useState, useEffect, useCallback } from "react";
+
+export const useCarouselControl = (interval) => {
+    const [autoplayTimeout, setAutoplayTimeout] = useState(null);
+    const [currentScreen, setCurrentScreen] = useState(0);
+
+    const [emblaRef, emblaApi] = useEmblaCarousel({
+        loop: true,
+        duration: 30,
+        watchDrag: true,
+        breakpoints: {
+            "(min-width: 128px)": {
+                watchDrag: true,
+            },
+        },
+    }, [Autoplay({
+        delay: interval,
+        stopOnInteraction: false, 
+        stopOnMouseEnter: false, 
+        rootNode: (emblaRoot) => emblaRoot.parentElement,
+    })])
+
+    const onButtonClick = useCallback(page => {
+        if(!emblaApi) return
+        const autoPlay = emblaApi?.plugins()?.autoplay;
+
+        if(!autoPlay) return
+
+        autoPlay.stop();
+        emblaApi.scrollTo(page);
+        setCurrentScreen(page);
+        
+        if(autoplayTimeout) {
+            clearTimeout(autoplayTimeout);
+        }
+
+        const autoplayResumeTimeout = setTimeout(() => {
+            autoPlay.play();
+            emblaApi.scrollNext();
+        }, interval);
+        setAutoplayTimeout(autoplayResumeTimeout);
+    }, [emblaApi, autoplayTimeout, interval])
+
+    useEffect(() => {
+        if(!emblaApi) return
+        emblaApi.on('select', () => setCurrentScreen(emblaApi.selectedScrollSnap()));
+    }, [emblaApi])
+
+    return {
+        onButtonClick,
+        currentScreen,
+        emblaRef
+    }
 }
